7e107f446b883d4845f439750b62475e0c80cd6d
Anindya CISPL <anindya.bhattacharya@codeclouds.in>
2021-01-22 22:05:01 +0530

    WIP

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -21,9 +21,9 @@ require('partials/header.view.php');
         ?>
             <a href="story?id=<?= $story->id; ?>">
                 <div class="item">
-                    <img src="<?= ltrim(getimage('story_id', $story->id, 1)[0]->location ??= './public/images/default.webp', '.'); ?>" alt="" width="200" height="250">
+                    <img src="<?= ltrim(getimage('story_id', $story->id, 1)[0]->location ??= './public/images/default.webp', '.'); ?>" alt="">
                     <p>Posted By <?= $story->name; ?></p>
-                    <h2><?= $story->title; ?></h2>
+                    <h4><?= $story->title; ?></h4>
                 </div>
             </a>
         <?php endforeach; ?>
